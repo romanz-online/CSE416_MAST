452a8d8e88c67535b83187bfd7be04217519a87f
roman <roman.zinoviev@stonybrook.edu>
2021-03-12 21:50:54 -0500

    Improved search functionality to redirect to the student index. also included capability of searching by both name and sbu id simultaneously (or one at a time).

--- mast/views.py
+++ mast/views.py
@@ -8,10 +8,13 @@ def index(request):
 
 
 def search(request):
-    search = request.GET['query']
-    student_list = Student.objects.filter(sbu_id__icontains=search)
-    context = {'student_list': student_list, 'search': search}
-    return render(request, 'mast/search.html', context)
+    name_search = request.GET['name']
+    sbu_id_search = request.GET['sbu_id']
+    name_list = Student.objects.filter(name__icontains=name_search)
+    sbu_id_list = Student.objects.filter(sbu_id__icontains=sbu_id_search)
+    student_list = list(set(name_list) & set(sbu_id_list))
+    context = {'student_list': student_list, 'name_search': name_search, 'sbu_id_search': sbu_id_search}
+    return render(request, 'mast/index.html', context)
 
 
 def detail(request, sbu_id):
