46c2cd6815aa5d1fec6ebb3dee1c2feea395191e
roman <roman.zinoviev@stonybrook.edu>
2021-03-13 17:31:36 -0500

    Needs some work to make it easier to use, but I added a 'show graduated/withdrawn students' functionality to the search. also added gpd and student groups and accounts via admin

--- mast/views.py
+++ mast/views.py
@@ -10,10 +10,23 @@ def index(request):
 def search(request):
     name_search = request.GET['name']
     sbu_id_search = request.GET['sbu_id']
+    graduated_search = False if request.GET.get('graduated', 'on') else True
+    withdrew_search = False if request.GET.get('withdrew', 'on') else True
     name_list = Student.objects.filter(name__icontains=name_search)
     sbu_id_list = Student.objects.filter(sbu_id__icontains=sbu_id_search)
+    if not graduated_search:
+        name_list = name_list.filter(graduated=False)
+        sbu_id_list = sbu_id_list.filter(graduated=False)
+    if not withdrew_search:
+        name_list = name_list.filter(withdrew=False)
+        sbu_id_list = sbu_id_list.filter(withdrew=False)
+
     student_list = list(set(name_list) & set(sbu_id_list))
-    context = {'student_list': student_list, 'name_search': name_search, 'sbu_id_search': sbu_id_search}
+    context = {'student_list': student_list,
+               'name_search': name_search,
+               'sbu_id_search': sbu_id_search,
+               'graduated_search': '',
+               'withdrew_search': ''}
     return render(request, 'mast/index.html', context)
 
 
