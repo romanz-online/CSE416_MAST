3835ead83758a98498e7d11c255fca3ca6a20046
roman <roman.zinoviev@stonybrook.edu>
2021-03-20 23:49:28 -0400

    tried to change the background but it didnt work

--- mast/views.py
+++ mast/views.py
@@ -276,7 +276,7 @@ def commit_edit(request, sbu_id):
 
 def get_grade_number(grade):
     dict = {'A': 4.0, 'A-': 3.7, 'B+': 3.3, 'B': 3.0, 'B-': 2.7, 'C+': 2.3, 'C': 2.0, 'C-': 1.7, 'D+': 1.3, 'D': 1.0,
-            'D-': 0.7, 'F': 0.0}
+            'D-': 0.7, 'F': 0.0, 'S': 4.0}
     return dict[grade]
 
 
