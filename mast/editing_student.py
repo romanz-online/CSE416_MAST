26a90c420e8f04abce81006f33dc8b19070e2310
Kevin Gabayan <kevin.gabayan@stonybrook.edu>
2021-03-29 17:37:34 -0400

    Documentation
    Added docstring and comments to edit class.

--- mast/editing_student.py
+++ mast/editing_student.py
@@ -4,11 +4,23 @@ from django.urls import reverse
 from .models import Student, Major, Course, Classes_Taken_by_Student, Grade, CourseStatus, Semester,\
     Requirement_Semester, Season
 
-
 def edit(request, sbu_id):
+    """
+    Retrieves and renders a specific student to be edited on the edit screen page
+    
+        Parameters:
+            request (HttpRequest): The request object used to pass states through the system. 
+            sbu_id (int): The SBU ID used to retrieve the student object. 
+
+        Returns:
+            render (HttpResponse): Returns the respective view containing the respective information of the student retrieved.     
+    """
+    # Retrieve student using respective SBU ID 
     student = get_object_or_404(Student, pk=sbu_id)
+    # Retrieve grade and course status list 
     grade_list = [i[0] for i in Grade.choices]
     course_status_list = [i[0] for i in CourseStatus.choices]
+    # Render view with student information 
     return render(request, 'mast/edit.html', {'student': student,
                                               'major_list': Major.objects.order_by('name'),
                                               'course_list': Course.objects.order_by('name'),
@@ -20,10 +32,23 @@ def edit(request, sbu_id):
 
 
 def delete_record(request, sbu_id):
+    """
+    Deletes a specific student record from the database.
+    
+        Parameters:
+            request (HttpRequest): The request object used to pass states through the system. 
+            sbu_id (int): The SBU ID used to retrieve the student object. 
+
+        Returns:
+            render (HttpResponse): Returns the respective view based on the flow of events upon attemping deletion.
+    """   
+    # Retrieve student object 
     student = get_object_or_404(Student, pk=sbu_id)
     try:
+        # Attempt to delete student 
         student.delete()
     except:
+        # Return an error message if the student could not be deleted.
         grade_list = [i[0] for i in Grade.choices]
         course_status_list = [i[0] for i in CourseStatus.choices]
         return render(request, 'mast/edit.html', {'student': student,
@@ -37,13 +62,27 @@ def delete_record(request, sbu_id):
                                                       'year'),
                                                   'error_message': "Something went wrong."
                                                   })
+    # If the deletion was successful, return to the home page 
     context = {'student_list': Student.objects.order_by('sbu_id'), 'major_list': Major.objects.order_by('name')}
     return render(request, 'mast/student_index.html', context)
 
 
 def commit_edit(request, sbu_id):
+    """
+    Commits an edit for a specific student. 
+    
+        Parameters:
+            request (HttpRequest): The request object used to pass states through the system. 
+            sbu_id (int): The SBU ID used to retrieve the student object. 
+
+        Returns:
+            render (HttpResponse): Returns an error message if the edits could not be committed successfully. 
+            HttpResponseRedirect: Redirects back to the student view if the edit was successful.
+    """    
+    # Retrieve student object 
     student = get_object_or_404(Student, pk=sbu_id)
     try:
+        # Attempt to make changes to their data
         first_name = request.GET['first_name']
         last_name = request.GET['last_name']
         email = request.GET['email']
@@ -94,6 +133,7 @@ def commit_edit(request, sbu_id):
 
         student.save()
     except:
+        # Return an error message if their data could not be saved 
         student = get_object_or_404(Student, pk=sbu_id)
         grade_list = [i[0] for i in Grade.choices]
         course_status_list = [i[0] for i in CourseStatus.choices]
@@ -112,14 +152,35 @@ def commit_edit(request, sbu_id):
 
 
 def get_grade_number(grade):
+    """
+    Retrieve the grade number of the student based on their letter grade
+
+        Parameters:
+            grade (str): The letter grade that the student received.
+
+        Returns:
+            dict[grade] (int): The numerical grade converted from the letter grade.
+    """
     dict = {'A': 4.0, 'A-': 3.7, 'B+': 3.3, 'B': 3.0, 'B-': 2.7, 'C+': 2.3, 'C': 2.0, 'C-': 1.7, 'D+': 1.3, 'D': 1.0,
             'D-': 0.7, 'F': 0.0, 'S': 4.0}
     return dict[grade]
 
 
 def add_taken_course(request, sbu_id):
+    """
+    Adds a taken course to the student database. 
+
+        Parameters:
+            request (HttpRequest): The request object used to pass states through the system. 
+            sbu_id (int): The SBU ID used to retrieve the student object. 
+
+        Returns:
+            HttpResponseRedirect: Redirects back to the student view based on the flow of events.
+    """
+    # Retrieve student object 
     student = get_object_or_404(Student, pk=sbu_id)
     try:
+        # Attempt to add taken course 
         new_course = request.GET['course']
         new_course = Course.objects.get(id=new_course)
         c = Classes_Taken_by_Student(student=student, course=new_course, grade='A')
@@ -130,8 +191,21 @@ def add_taken_course(request, sbu_id):
 
 
 def modify_course_in_progress(request, sbu_id, record):
+    """
+    Modifies a current course in progress to their current state.
+
+        Parameters:
+            request (HttpRequest): The request object used to pass states through the system. 
+            sbu_id (int): The SBU ID used to retrieve the student object. 
+            record (Object): Contains the specific id of the specific record in the list of classes taken.
+
+        Returns:
+            HttpResponseRedirect: Redirects back to the student view based on the flow of events.
+    """
+    # Retrieve student object
     student = get_object_or_404(Student, pk=sbu_id)
     try:
+        # Attempt to modify course in progress 
         r = Classes_Taken_by_Student.objects.get(id=record)
         if request.GET['action'] == 'pass':
             r.status = CourseStatus.PASSED
