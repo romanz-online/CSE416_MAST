719be5d8f165d1ef8b4b4ede63718e95a9a0b51f
Emily Barker <emily.barker@stonybrook.edu>
2021-04-25 18:27:29 -0400

    Smart_Suggest steps 1-4

--- mast/smart_suggest.py
+++ mast/smart_suggest.py
@@ -2,6 +2,9 @@ from .models import Student, Major, Season, CoursesTakenByStudent, Comment, Stud
     TrackCourseSet, CourseInTrackSet, CourseToCourseRelation, Course, CoursePrerequisiteSet, Prerequisite, \
     CourseInstance, CourseStatus
 
+from modifying_schedule import sort_semester_list
+
+
 #main smart_suggest driver
 def smart_suggest(student):
     #get graduated students who have same track and major
@@ -12,27 +15,93 @@ def smart_suggest(student):
     #else find similar students
     else: 
         similar_schedules = calculate_similarity(student, graduate_set)
+    #if no similar_schedules
+    if len(similar_schedules) == 0:
+        #will need to be updated to a proper render request once UI is in
+        return "Not enough data for Smart Suggest"
+    #else continue and get course counts
+    course_counts = course_counts(student, graduate_set)
 
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~HELPER FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 #calculate similarity between students, and return students with 80% or higher
 def calculate_similarity(student, graduate_set):
     student_courses = StudentCourseSchedule.object.filter(student=student)
     student_semesters = map_semester_numbers(student)
+    #get dict of classes mapped to semester number
+    student_dict = coures_semester_map(student_courses, student_semesters)
+    #compare to each graduated student
     for g_s in graduate_set:
         graduated_schedule = StudentCourseSchedule.object.filter(student=g_s)
+        graduated_semesters = map_semester_numbers(g_s)
+        graduated_dict = coures_semester_map(graduated_schedule, graduated_semesters)
+        #keep track of all matching classes
         same_class_in_same_semester = 0
         same_class_in_different_semester = 0
-        class_not_taken = 0 
-        #for course in student_courses:
+        same_class_in_different_semester = 0 
+        #find relation to current student course
+        for course in student_dict:
+            if course in graduated_dict:
+                if graduated_dict.get(course) == student_dict.get(course):
+                    same_class_in_same_semester += 1
+                else:
+                    same_class_in_different_semester += 1
+            else:
+                class_not_taken += 1
+
+        similarity = (same_class_in_same_semester + (0.5*same_class_in_different_semester) ) / len(student_courses)
+        #if similarity is less than 80%, remove graduated student from list
+        if similarity < .8:
+            graduate_set.remove(g_s)
+
+    return graduate_set
 
 
 #get dictionary that maps each semester a student has taken courses in to an ordered number
 def map_semester_numbers(student):
-    #dictionary mapping each season to its order number
-    seasons = {'Winter':1, 'Spring':2, 'Summer':3, 'Fall':4}
     semester_map = {}
-    first_year = StudentCourseSchedule.object.filter(student=student).aggregate(Min(''))
+    #list of all semesters which student has taken classes in 
+    semesters = []
+    first_year = StudentCourseSchedule.object.filter(student=student)
+    for course_instance in student_courses:
+        if course_instance.course.semester not in semesters:
+            semesters.append(course_instance.course.semester)
+
+    #use function in modyfying schedule to sort semesters
+    semesters = sort_semester_list(semesters)
+    #dictionary with value as order number
+    for i in range(len(semesters)):
+        semester_map[semesters[i]] = i
+
+#get dictionary that maps all classes a student has taken to its semester number
+def coures_semester_map(student_courses, student_semesters):
+    student_dict = {}
     for course in student_courses:
+        student_dict[course.course.name] = student_semesters.get(course.course.semester)  
+    return student_dict
 
 
+#get dictionary of course counts from similar students that the current student has not yet taken
+def course_counts(student, graduate_set):
+    course_counts = {}
+    student_courses = StudentCourseSchedule.object.filter(student=student)
+    taken_course_list = []
+    #get list of names for easy comparison
+    for course in student_courses:
+        taken_course_list.append(course.course.name)
+    
+    for g_s in graduate_set:
+        graduated_courses = StudentCourseSchedule.object.filter(student=g_s)
+        graduated_semesters = map_semester_numbers(g_s)
+        graduated_map = coures_semester_map(graduated_courses, graduated_semesters)
+        for course in graduated_map:
+            #get count for all classes not yet taken by current student
+            if course.course.name not in taken_course_list
+                course_semester_key = course.course.name + ", " + str(graduated_map.get_value(course))
+                if course_semester_key in course_counts:
+                    course_counts[course_semester_key] += 1
+                else:
+                    course_counts[course_semester_key] = 1
+
+    return course_counts
+     
