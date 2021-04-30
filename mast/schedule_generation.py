9fa6c59f8278f43b0e2bb138f9a926e7cd2f56df
roman <32982682+romanz-online@users.noreply.github.com>
2021-04-30 00:11:49 -0400

    started working on the schedule generation ui

--- mast/schedule_generation.py
+++ mast/schedule_generation.py
@@ -1,11 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Student
+from .models import Student, CourseInstance
 
 
 def schedule_generation(request, sbu_id):
     student = get_object_or_404(Student, pk=sbu_id)
+    course_list = {i for i in CourseInstance.objects.all() if i.section != 999}
     context = {
         'student': student,
+        'course_list': course_list
     }
     return render(request, 'mast/schedule_generation.html', context)
