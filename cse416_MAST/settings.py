0d083e13a2662a35e336a2ffbfc99c6e4a2cd3da
Kevin Gabayan <kevin.gabayan@stonybrook.edu>
2021-05-02 19:07:15 -0400

    heroku trial 5

--- cse416_MAST/settings.py
+++ cse416_MAST/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 import django_heroku 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
