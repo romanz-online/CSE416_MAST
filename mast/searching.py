fa2049352ccb98713782a515d4d115ebc01603c4
Kevin Gabayan <kevin.gabayan@stonybrook.edu>
2021-05-03 17:01:58 -0400

    Revert "fixed sorting not working"
    This reverts commit 4a13467317a0a65ac54f8bf0be800dd90c956366.

--- mast/searching.py
+++ mast/searching.py
@@ -52,10 +52,10 @@ def search(request):
     name_search = request.GET['name']
     sbu_id_search = request.GET['sbu_id']
     major_search = request.GET['major']
-    graduated_search = int(request.GET['graduated'])
-    withdrew_search = int(request.GET['withdrew'])
-    plan_complete_search = int(request.GET['plan_complete'])
-    plan_valid_search = int(request.GET['plan_valid'])
+    graduated_search = request.GET['graduated']
+    withdrew_search = request.GET['withdrew']
+    plan_complete_search = request.GET['plan_complete']
+    plan_valid_search = request.GET['plan_valid']
     first_name_list = Student.objects.filter(first_name__icontains=name_search)
     last_name_list = Student.objects.filter(last_name__icontains=name_search)
     sbu_id_list = Student.objects.filter(sbu_id__icontains=sbu_id_search)
@@ -116,7 +116,6 @@ def search(request):
                 found = True
         if not found:
             track_list.append(i)
-    print(withdrew_search)
     context = {'student_list': student_list,
                'major_list': Major.objects.order_by('name'),
                'name_search': name_search,
